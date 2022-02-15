87977eb909fe24ff4ec1a3e9a1d11c0278a7c4cb
taranovska <93533926+taranovska@users.noreply.github.com>
2022-02-15 12:28:28 +0200

    add pagination logic

--- src/Follower.js
+++ src/Follower.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Follower = () => {
-  return <h2>follower component</h2>
-}
+const Follower = (props) => {
+  const { avatar_url, html_url, login } = props;
+  return (
+    <section className="card">
+      <img src={avatar_url} alt={login} />
+      <h4>{login}</h4>
+      <a href={html_url} className="btn">
+        view profile
+      </a>
+    </section>
+  );
+};
 
-export default Follower
+export default Follower;
